810812fee26ef0b79eaffa3da50e7f7a51d9d02c
ibrahim378Edx <werdewer@yahoo.com>
2023-07-23 02:11:00 +0300

    first test

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int _printf(const char *z, ...);
+int counter(const char *z);
+int specifierS(char *str, int q);
+int specifierC(char c, int q);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,105 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _printf -  trying to replicate printf
+ *
+ * @z: string provided by user
+ *
+ * Return: amount of chars printed
+ */
+int  _printf(const char *z, ...)
+{
+va_list args;
+int q;
+q = counter(z);
+va_start(args, z);
+	while (*z != '\0')
+	{
+		if (*z == '%')
+		{
+			z++;
+			if (*z == 'c')
+			{
+				char c = (char) va_arg(args, int);
+
+				q = specifierC(c, q);
+			}
+			else if (*z == 's')
+			{
+				char *str = va_arg(args, char*);
+
+				q = specifierS(str, q);
+			}
+			else if (*z == '%')
+			{
+				char percent;
+
+				percent = '%';
+				write(STDOUT_FILENO, &percent, 1);
+				q = q - 2;
+				q++;
+			}
+
+		}
+write(STDOUT_FILENO, z, sizeof(*z));
+z++;
+	}
+return (q);
+}
+
+/**
+ * counter - counter for the full string
+ *
+ * @z: string provided by user
+ *
+ * Return: the string length
+ */
+int counter(const char *z)
+{
+int i;
+i = 0;
+	while (*(z + i) != '\0')
+	{
+	i++;
+	}
+return (i);
+}
+/**
+ * specifierC - Specifier for %s
+ *
+ * @c: char to print
+ *
+ * @q: counter of chars printed
+ *
+ * Return: returing q again after updating
+*/
+int specifierC(char c, int q)
+{
+write(STDOUT_FILENO, &c, sizeof(char));
+q++;
+q = q - 2;
+return (q);
+}
+/**
+ * specifierS - Specifier for %c
+ *
+ * @str: string to print
+ *
+ * @q: counter of chars printed
+ *
+ * Return: returing q again after updating
+*/
+int specifierS(char *str, int q)
+{
+int counterr;
+counterr = 0;
+q = q - 2;
+while (*str != '\0')
+{
+	counterr++;
+	str++;
+	q++;
+}
+write(STDOUT_FILENO, str, sizeof(char) * counterr);
+return (q);
+}

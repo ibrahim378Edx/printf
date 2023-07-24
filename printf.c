4e30b4e4f7ed2c84329b954412bb862573945692
ibrahim378Edx <werdewer@yahoo.com>
2023-07-24 10:48:17 +0300

    test

--- printf.c
+++ printf.c
@@ -1,108 +1,94 @@
 #include "main.h"
 #include <unistd.h>
 /**
+ * _printf -  trying to replicate printf
+ *
+ * @z: string provided by user
+ *
+ * Return: amount of chars printed
+ */
+int  _printf(const char *z, ...)
+{
+va_list args;
+int q;
+q = 0;
+va_start(args, z);
+if (z == NULL)
+	return (-1);
 
-    _printf - trying to replicate printf
+while (*z != '\0')
+{
+	if (*z == '%')
+	{
+		z++;
+		if (*z == 'c')
+		{
+			char c = (char) va_arg(args, int);
 
-    @z: string provided by user
+			q = specifierC(c, q);
+			z++;
+		}
+		else if (*z == 's')
+		{
+			char *str = va_arg(args, char*);
 
-    Return: amount of chars printed
-    */
-    int _printf(const char *z, ...)
-    {
-    va_list args;
-    int q;
-    q = 0;
-    va_start(args, z);
-    while (*z != '\0')
-    {
-    if (*z == '%')
-    {
-    z++;
-    if (*z == 'c')
-    {
-    char c = (char) va_arg(args, int);
-    ini
-
-     		q = q + specifierC(c, q);
-     		z++;
-     	}
-     	else if (*z == 's')
-     	{
-     		char *str = va_arg(args, char*);
-
-     		q = q + specifierS(str, q);
-     		z++;
-     	}
-     	else if (*z == '%')
-     	{
-
-     		write(STDOUT_FILENO, z, 1);
-     		q++;
-     		z++;
-     	}
-
-     }
-     else
-     {
-     	write(STDOUT_FILENO, z, sizeof(char));
-     	q++;
-     	z++;
-     }
-
-    }
-    printf("%d", q);
-    return (q);
-    }
-
+			q = specifierS(str, q);
+			z++;
+		}
+		else if (*z == '%')
+		{
+			write(STDOUT_FILENO, z, 1);
+			q++;
+			z++;
+		}
+	}
+	else
+	{
+		write(STDOUT_FILENO, z, sizeof(char));
+		q++;
+		z++;
+	}
+}
+va_end(args);
+return (q);
+}
 /**
-
-    counter - counter for the full string
-    @z: string provided by user
-    Return: the string length
-    /
-    int counter(const char z)
-    {
-    int i;
-    i = 0;
-    while ((z + i) != '\0')
-    {
-    i++;
-    }
-    return (i);
-    }
-    /*
-    specifierC - Specifier for %s
-    @c: char to print
-    @q: counter of chars printed
-    Return: returing q again after updating
-    /
-    int specifierC(char c, int q)
-    {
-    write(STDOUT_FILENO, &c, sizeof(char));
-    q++;
-    return (q);
-    }
-    /*
-    specifierS - Specifier for %c
-    @str: string to print
-    @q: counter of chars printed
-    Return: returing q again after updating
-    */
-    int specifierS(char *str, int q)
-    {
-    char *w = "(null)";
-    if (str != NULL)
-    {
-    while (*str != '\0')
-    {
-    write(STDOUT_FILENO, str, sizeof(char));
-    str++;
-    q++;
-    }
-    return (q);
-    }
-    write(STDOUT_FILENO, w, sizeof(char) * 6);
-    return (6);
-    }
-    i mean this code check it
+ * specifierC - Specifier for %s
+ *
+ * @c: char to print
+ *
+ * @q: counter of chars printed
+ *
+ * Return: returing q again after updating
+*/
+int specifierC(char c, int q)
+{
+write(STDOUT_FILENO, &c, sizeof(char));
+q++;
+return (q);
+}
+/**
+ * specifierS - Specifier for %c
+ *
+ * @str: string to print
+ *
+ * @q: counter of chars printed
+ *
+ * Return: returing q again after updating
+*/
+int specifierS(char *str, int q)
+{
+char *w = "(null)";
+if (str != NULL)
+{
+while (*str != '\0')
+{
+	write(STDOUT_FILENO, str, sizeof(char));
+	str++;
+	q++;
+}
+return (q);
+}
+write(STDOUT_FILENO, w, sizeof(char) * 6);
+return (q + 6);
+}
